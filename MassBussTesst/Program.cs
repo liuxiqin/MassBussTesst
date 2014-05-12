70e28b4b467791117b212d8cb9e78774dde3ee46
malin <malin@piatka.pl>
2014-05-12 14:13:30 +0200

    Exe odpala testy (poza 1 - niedeterministycznym)

--- MassBussTesst/Program.cs
+++ MassBussTesst/Program.cs
@@ -1,14 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace MassBussTesst
 {
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var suite = new SzynaTests();
+            RunTest(suite, o => o.WysłanyKomunikatDocieraDoCelu());
+            RunTest(suite, o => o.PozwalaWysyłaćSekwencyjnie());
+            RunTest(suite, o => o.WysyłanieJestTransakcyjne_PoRollbackuKomunikatNieDociera());
+            RunTest(suite, o => o.WPrzypadkuBłęduPrzyOdbieraniuKomunikatWracaIJestPonawiany());
+            //RunTest(suite, o => o.WPrzypadkuPonawianiaKolejnośćKomunikatówNieJestZachowana());
+            RunTest(suite, o => o.MożnaWymusićSekwencyjnośćNawetWPrzypadkuPonawiania());
+        }
+
+        private static void RunTest(SzynaTests suite, Action<SzynaTests> test)
+        {
+            try
+            {
+                suite.SetUpEachTest();
+                test(suite);
+            }
+            catch (Exception)
+            {
+                suite.TearDownEachTest();
+                throw;
+            }
         }
     }
 }
